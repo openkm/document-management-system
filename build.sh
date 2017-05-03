39a00aecb24dccb57717c719a90967200503a567
Paco Avila <pavila@openkm.com>
2017-05-03 19:49:48 +0200

    Improvements for release process

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-YELLOW='\e[1;33m';
-RESET='\e[0m';
+YELLOW='\e[1;33m'
+RESET='\e[0m'
 
 mvn -Dmaven.test.skip=true clean gwt:compile install $*
+
+echo -e ${YELLOW} "Community version compiled" ${RESET}
---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+BLUE='\e[1;34m'
+RESET='\e[0m'
+VERSION="6.3.5"
+ARCHIVE="OpenKM-${VERSION}.zip"
+
+# Build package
+echo -e ${BLUE} "** Build $ARCHIVE..." ${RESET}
+cd target
+md5sum OpenKM.war > md5sum.txt
+zip $ARCHIVE OpenKM.war md5sum.txt
