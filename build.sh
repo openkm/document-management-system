1f2a03a0d9f56b5b16d4e4fc7e0534782dfcdee2
Paco Avila <pavila@openkm.com>
2018-03-01 11:55:02 +0100

    Support for Maven Wrapper

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 YELLOW='\e[1;33m'
 RESET='\e[0m'
 
-mvn -Dmaven.test.skip=true clean gwt:compile install $*
+./mvnw -Dmaven.test.skip=true clean gwt:compile install $*
 
 echo -e ${YELLOW} "Community version compiled" ${RESET}
