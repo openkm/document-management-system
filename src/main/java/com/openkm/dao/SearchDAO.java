72a00b788d65240e1ddcb556160ab34fbbe85c65
Sergio Antonio Ochoa Martinez <gnu.java.sergio@gmail.com>
2020-04-05 03:02:59 -0400

    Extend SearchService API (#226)
    Fix #223
    
    Co-authored-by: Sergio Ochoa <sochoa@openkm.com>

--- src/main/java/com/openkm/dao/SearchDAO.java
+++ src/main/java/com/openkm/dao/SearchDAO.java
@@ -241,7 +241,7 @@ public class SearchDAO {
 	 */
 	@SuppressWarnings("unchecked")
 	private NodeResultSet runQueryLucene(FullTextSession ftSession, Query query, int offset, int limit)
-			throws IOException, InvalidTokenOffsetsException, HibernateException {
+			throws IOException, InvalidTokenOffsetsException, HibernateException, DatabaseException {
 		log.debug("runQueryLucene({}, {}, {}, {})", new Object[]{ftSession, query, offset, limit});
 		List<NodeQueryResult> results = new ArrayList<NodeQueryResult>();
 		NodeResultSet result = new NodeResultSet();
@@ -492,7 +492,7 @@ public class SearchDAO {
 	 * Add result
 	 */
 	private void addResult(FullTextSession ftSession, List<NodeQueryResult> results, Highlighter highlighter, Float score,
-			NodeBase nBase)	throws IOException, InvalidTokenOffsetsException {
+			NodeBase nBase)	throws IOException, InvalidTokenOffsetsException, DatabaseException {
 		NodeQueryResult qr = new NodeQueryResult();
 		NodeDocument nDocument = null;
 		NodeMail nMail = null;
@@ -501,7 +501,7 @@ public class SearchDAO {
 		if (nBase instanceof NodeDocument) {
 			nDocument = (NodeDocument) nBase;
 
-			if (NodeMailDAO.getInstance().isMail(ftSession, nDocument.getParent())) {
+			if (NodeMailDAO.getInstance().itemExists(nDocument.getParent())) {
 				log.debug("NODE DOCUMENT - ATTACHMENT");
 				qr.setAttachment(nDocument);
 			} else {
