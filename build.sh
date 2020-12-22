1aaa1f2cade7115a4553e51917b70269a426f9f5
Paco Avila <pavila@openkm.com>
2020-12-22 10:29:47 +0100

    Release v6.3.10

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 YELLOW='\e[1;33m'
 RESET='\e[0m'
 
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+# export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 mvn -Dmaven.test.skip=true clean gwt:compile install $*
 
 echo -e "${YELLOW}Community version compiled${RESET}"
